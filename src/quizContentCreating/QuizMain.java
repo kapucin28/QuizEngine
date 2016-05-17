8f1fe8487b61a4784f42558655f0981e1fc28e2a
Timbuli Remus <kapucin28@gmail.com>
2016-05-17 13:09:47 +0300

    Initial commit

---
+++ src/quizContentCreating/QuizMain.java
@@ -0,0 +1,42 @@
+package quizContentCreating;
+
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+/**
+ * Created by TIMBULI REMUS K@puc!n on 17-May-16.
+ */
+public class QuizMain extends Application {
+
+    // Stage variables--------------------------------------------------------------------------------------------------
+    private Stage stage;
+    private final Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+    private final Pane pane = new Pane();
+    private final Scene scene = new Scene(pane, bounds.getWidth() / 2, bounds.getHeight() / 2);
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Main method------------------------------------------------------------------------------------------------------
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Start method-----------------------------------------------------------------------------------------------------
+    public void start(Stage stage){
+        this.stage = stage;
+        layoutSetup();
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Stage layout setup-----------------------------------------------------------------------------------------------
+    private void layoutSetup(){
+        stage.setTitle("Quiz Maker");
+        stage.setScene(scene);
+        stage.show();
+    }
+    //------------------------------------------------------------------------------------------------------------------
+}
