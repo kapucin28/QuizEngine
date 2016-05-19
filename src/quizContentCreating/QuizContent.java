e1fd613c208f70705e34a36f5008a205f9c2599f
Timbuli Remus <kapucin28@gmail.com>
2016-05-19 13:02:37 +0300

    Creating question content

--- src/quizContentCreating/QuizContent.java
+++ src/quizContentCreating/QuizContent.java
@@ -4,4 +4,42 @@ package quizContentCreating;
  * Created by TIMBULI REMUS K@puc!n on 17-May-16.
  */
 public class QuizContent {
+
+    // Table content variables------------------------------------------------------------------------------------------
+    private String question, answer, result;
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Constructor------------------------------------------------------------------------------------------------------
+    public QuizContent(String question, String answer, String result) {
+        this.question = question;
+        this.answer = answer;
+        this.result = result;
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Getters & Setters------------------------------------------------------------------------------------------------
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+    //------------------------------------------------------------------------------------------------------------------
 }
