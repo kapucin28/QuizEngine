679af928eb5aa9f29db7a6acd94e00cae14c72fb
Timbuli Remus <kapucin28@gmail.com>
2016-05-20 14:05:08 +0300

    Adding table variables

--- src/quizRendering/QuizMaker.java
+++ src/quizRendering/QuizMaker.java
@@ -1,19 +1,32 @@
 package quizRendering;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
+import quizContentCreating.QuizContent;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 17-May-16.
  */
-public class QuizMaker extends Pane{
+class QuizMaker extends Pane{
 
     // Score & user actions variables-----------------------------------------------------------------------------------
     private Label score = new Label();
     private Label pointsLabel = new Label("points");
     private final Button newQuiz = new Button("New Quiz");
     private final Button submit = new Button("Submit");
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Table variables--------------------------------------------------------------------------------------------------
+    private TableView<QuizContent> tableView = new TableView<>();
+    private TableColumn<QuizContent, String> questionColumn = new TableColumn<>("Question");
+    private TableColumn<QuizContent, String> answerColumn = new TableColumn<>("Answer");
+    private TableColumn<QuizContent, String> resultColumn = new TableColumn<>("Result");
+    private ObservableList<QuizContent> list = FXCollections.observableArrayList();
     //------------------------------------------------------------------------------------------------------------------
 
 }
