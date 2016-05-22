5d56400f3579d955e80fde9727ea39056be32f6e
Timbuli Remus <kapucin28@gmail.com>
2016-05-22 14:58:14 +0300

    Adding write questions to file method

--- src/quizContentCreating/CreatingQuestions.java
+++ src/quizContentCreating/CreatingQuestions.java
@@ -1,7 +1,45 @@
 package quizContentCreating;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  * Created by TIMBULI REMUS K@puc!n on 17-May-16.
  */
 public class CreatingQuestions {
+
+    // Managing questions variables-------------------------------------------------------------------------------------
+    private String question;
+    private static final String path = "Desktop/quiz.dat";  // Any location & filename
+    private static RandomAccessFile file;
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Constructor------------------------------------------------------------------------------------------------------
+    public CreatingQuestions(){
+
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Writing questions to file----------------------------------------------------------------------------------------
+    private static void writeToFile(String path, String data, int position){
+        try {
+            file = new RandomAccessFile(path, "rw");
+            file.seek(position);
+            file.write(data.getBytes());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Getters & Setters------------------------------------------------------------------------------------------------
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    //------------------------------------------------------------------------------------------------------------------
 }
