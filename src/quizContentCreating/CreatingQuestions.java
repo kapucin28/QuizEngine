bf379fbf4325f1929b9af0a6d77c45d031cb9465
Timbuli Remus <kapucin28@gmail.com>
2016-05-24 11:01:57 +0300

    Updating random questions method

--- src/quizContentCreating/CreatingQuestions.java
+++ src/quizContentCreating/CreatingQuestions.java
@@ -10,19 +10,21 @@ public class CreatingQuestions {
 
     // Managing questions variables-------------------------------------------------------------------------------------
     private String question;
+    private String[] questions;
     private static byte[] bytes;
     private static final String path = "Desktop/quiz.dat";  // Any location & filename
     private static RandomAccessFile file;
     //------------------------------------------------------------------------------------------------------------------
 
     // Constructor------------------------------------------------------------------------------------------------------
-    public CreatingQuestions(){
+    public CreatingQuestions() {
         quizQuestions();
+        readQuestions();
     }
     //------------------------------------------------------------------------------------------------------------------
 
     // Writing questions to file method---------------------------------------------------------------------------------
-    private static void writeToFile(String path, String data, int position){
+    private static void writeToFile(String path, String data, int position) {
         try {
             file = new RandomAccessFile(path, "rw");
             file.seek(position);
@@ -35,7 +37,7 @@ public class CreatingQuestions {
     //------------------------------------------------------------------------------------------------------------------
 
     // Reading questions method-----------------------------------------------------------------------------------------
-    private static byte[] readFromFile(String path, int position, int size){
+    private static byte[] readFromFile(String path, int position, int size) {
         try {
             file = new RandomAccessFile(path, "r");
             file.seek(position);
@@ -50,12 +52,24 @@ public class CreatingQuestions {
     //------------------------------------------------------------------------------------------------------------------
 
     // Questions--------------------------------------------------------------------------------------------------------
-    private void quizQuestions(){
+    private void quizQuestions() {
         writeToFile(path, "1 = 1", 0);
         writeToFile(path, "1 != 1", 6);
         writeToFile(path, "Java = programming language", 13);
         writeToFile(path, "Java != programming language", 41);
         writeToFile(path, "This is a cool app", 69);
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Reading questions method-----------------------------------------------------------------------------------------
+    private void readQuestions() {
+        questions = new String[]{new String(readFromFile(path, 0, 5)),
+                new String(readFromFile(path, 6, 6)),
+                new String(readFromFile(path, 13, 27)),
+                new String(readFromFile(path, 41, 28)),
+                new String(readFromFile(path, 69, 18))
+        };
+        question = questions[(int) (Math.random() * 12)];
     }
     //------------------------------------------------------------------------------------------------------------------
 
