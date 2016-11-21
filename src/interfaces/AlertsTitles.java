c2794147d8d51c050a49eb827575183af6ef7acf
kapucin28 <kapucin28@gmail.com>
2016-11-22 00:25:37 +0200

    Implementing interface

---
+++ src/interfaces/AlertsTitles.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+/**
+ * Created by Timbuli Remus on 22.11.2016.
+ * <p>
+ * Interface for alerts
+ */
+public interface AlertsTitles {
+
+    String emptyAlertMessage = "Empty Alert";
+    String exitProgramMessage = "Exit Program";
+
+}
