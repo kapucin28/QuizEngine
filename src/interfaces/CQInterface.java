96621a5aa9605c29c35115b9b8778c0f0336ae31
kapucin28 <kapucin28@gmail.com>
2016-11-23 00:08:13 +0200

    Implementing interface

---
+++ src/interfaces/CQInterface.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+/**
+ * Created by Timbuli Remus on 23.11.2016.
+ * <p>
+ * Interface for creating questions
+ */
+public interface CQInterface {
+
+    String savingPath = "D:/Downloads/quiz.dat";
+    String readWrite = "rw";
+    String read = "r";
+    String q1 = "1 = 1";
+    String q2 = "1 != 1";
+    String q3 = "Java = programming language";
+    String q4 = "Java != programming language";
+    String q5 = "This is a cool app";
+
+}
